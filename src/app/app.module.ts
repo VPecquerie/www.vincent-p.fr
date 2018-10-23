00484cdf680202b30e7dc63970fdc9e862588a8e
VPecquerie <vincent.pecquerie@gmail.com>
2018-10-23 15:47:03 +0200

    feat(app): extract raven and routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, ErrorHandler} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
-import * as Raven from 'raven-js';
+
 
 import { AppComponent } from './app.component';
-import {TransferHttpCacheModule} from '@nguniversal/common';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 import { RavenErrorHandler } from './services/errors/raven.error.handler';
 import { MyMaterialModule } from './my-material/my-material.module';
 
-import { HomeComponent} from './home/home.component';
-
-Raven
-  .config('https://67482976a2124dd8bffbebaa5aa2eee6@sentry.we-program-it.com/2')
-  .install();
+import { HomeComponent } from './home/home.component';
+import { HomeTopbarComponent } from './home/topbar/home.topbar.component';
+import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full'},
-    ]),
-    TransferHttpCacheModule,
-    MyMaterialModule
-  ],
-  providers: [ { provide: ErrorHandler, useClass: RavenErrorHandler } ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomeComponent, HomeTopbarComponent],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'my-app' }),
+        TransferHttpCacheModule,
+        MyMaterialModule,
+        RouterModule.forRoot(routes, { enableTracing: true }),
+    ],
+    providers: [{ provide: ErrorHandler, useClass: RavenErrorHandler }],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
+import * as Raven from 'raven-js';
 
+Raven.config(
+  'https://67482976a2124dd8bffbebaa5aa2eee6@sentry.we-program-it.com/2'
+).install();
 
 if (environment.production) {
   enableProdMode();
