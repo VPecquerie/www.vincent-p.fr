65831d038c020188f1b3b4a39b6e893d25938bd8
VPecquerie <vincent.pecquerie@gmail.com>
2018-10-23 15:48:25 +0200

    feat(home): implement basic components.

---
+++ src/app/home/topbar/home.topbar.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-home-topbar',
+    templateUrl: './home.topbar.component.html',
+    styleUrls: ['./home.topbar.component.scss']
+})
+export class HomeTopbarComponent {
+
+}
