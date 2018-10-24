dca9ceac47136f995ad07b9d63ca7f490d2d0223
VPecquerie <vincent.pecquerie@gmail.com>
2018-10-24 09:46:50 +0200

    feat(splash): disable loading mode when content is loaded.

--- src/app/home/splashscreen/home.splashscreen.component.ts
+++ src/app/home/splashscreen/home.splashscreen.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomHttpHandler } from '../../services/errors/custom.http.handler';
 
 @Component({
     selector: 'app-home-splashscreen',
     templateUrl: './home.splashscreen.component.html',
     styleUrls: ['./home.splashscreen.component.scss'],
 })
-export class HomeSplashScreenComponent {
+export class HomeSplashScreenComponent implements OnInit {
     public isLoading = true;
+
+    ngOnInit(): void {
+        CustomHttpHandler.onLoad.subscribe(value => {
+            if (value === 0) {
+                this.isLoading = false;
+            }
+        });
+    }
 }
