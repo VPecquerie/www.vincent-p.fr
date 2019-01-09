f18ca0b828cd4ac259983d976b4686a366741e14
VPecquerie <vincent.pecquerie@gmail.com>
2019-01-09 13:37:39 +0100

    chore(matomo): add Configuration Properties.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,4 +13,8 @@ export const environment = {
     sentry: {
         configUrl: 'https://7cc4af3f1927461ea8abb8fd603fe6ce@sentry.io/1315438',
     },
+    matomo: {
+        url: 'matomo.vincent-p.fr',
+        id: 1
+    }
 };
