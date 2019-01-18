aebd61603487dc9bf1d79c24746c78e806d767e7
VPecquerie <vincent.pecquerie@gmail.com>
2019-01-18 13:49:52 +0100

    feat(perf): optimize bundle size thanks to webpack-bundle-analyzer

--- src/app/utils/passwordValidator.ts
+++ src/app/utils/passwordValidator.ts
@@ -1,5 +1,4 @@
 import { FormGroup } from '@angular/forms';
-import * as zxcvbn from 'zxcvbn';
 
 export class PasswordValidator {
     static MatchPassword(passwordFormSubGroup: FormGroup) {
@@ -21,7 +20,36 @@ export class PasswordValidator {
     }
 
     private static isEnoughSecure(value: any) {
-        const securityCheck = zxcvbn(value);
-        return securityCheck.score < 3; // @TODO: Store this constant somewhere else.
+        return this.scorePassword(value) >= 30;
+    }
+
+    private static scorePassword(password: string): number {
+        let score = 0;
+        if (!password) {
+            return score;
+        }
+        // award every unique letter until 5 repetitions
+        const letters = {};
+        for (let i = 0; i < password.length; i++) {
+            letters[password[i]] = (letters[password[i]] || 0) + 1;
+            score += 5.0 / letters[password[i]];
+        }
+
+        // bonus points for mixing it up
+        const variations = {
+            digits: /\d/.test(password),
+            lower: /[a-z]/.test(password),
+            upper: /[A-Z]/.test(password),
+            nonWords: /\W/.test(password),
+        };
+
+        let variationCount = 0;
+        for (const check in variations) {
+            if (variations.hasOwnProperty(check)) {
+                variationCount += (variations[check] === true) ? 1 : 0;
+            }
+        }
+        score += (variationCount - 1) * 10;
+        return score;
     }
 }
