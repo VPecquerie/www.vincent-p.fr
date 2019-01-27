efaedb6b00731de8d838b4acf299db4653632cf6
Vincent PECQUERIE <contact@vincent-p.fr>
2019-01-27 18:52:51 +0100

    chore(perf): use PM2 to run multiple thread.

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name: 'front',
+    script: 'dist/server.js',
+    args: '',
+    instances: 8,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
