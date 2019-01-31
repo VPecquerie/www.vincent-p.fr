723dcab7dfd4817892d27b7eefb3d6a6a65c32c8
Vincent PECQUERIE <vincent.pecquerie@trsb.net>
2019-01-31 15:45:51 +0100

    feat(admin): add some message when access is denied.

--- src/app/admin/admin.authgard.ts
+++ src/app/admin/admin.authgard.ts
@@ -1,19 +1,32 @@
 import { UserService } from '../services/user.service';
 import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { NotificationService } from '../services/notification.service';
 
 @Injectable()
 export class AdminAuthgard implements CanActivate {
 
-    constructor(private userService: UserService, private router: Router) {
+    constructor(private userService: UserService,
+                private router: Router,
+                private notificationService: NotificationService) {
     }
 
     canActivate() {
         const user = this.userService.getUser();
         if (!user) {
+
+            this.notificationService.Warning(
+                'Accès restreint !',
+                'Cette page est restreinte aux utilisateurs authentifé. Merci de vous authentifier pour accéder à son contenu.'
+            );
+
             this.router.navigate(['/admin/login']);
             return false;
         } else if (user.Token.hasExpired()) {
+            this.notificationService.Warning(
+                'Votre session a expiré !',
+                'Votre session a expiré. Merci de saisir à nouveau vos identifiants de connexion.'
+            );
             this.router.navigate(['/admin/login']);
             return false;
         } else {
