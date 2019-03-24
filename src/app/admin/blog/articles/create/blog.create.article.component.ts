c6bdad0d6d530318c6d2a5db1ce6b7ea1e36bf27
Vincent PECQUERIE <contact@vincent-p.fr>
2019-03-24 10:44:57 +0100

    feat(article): fill the add article form.

--- src/app/admin/blog/articles/create/blog.create.article.component.ts
+++ src/app/admin/blog/articles/create/blog.create.article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogCreateArticleForm } from './blog.create.article.form';
 
 @Component({
   selector: 'app-create',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.create.article.component.scss']
 })
 export class BlogCreateArticleComponent implements OnInit {
+    articleForm = BlogCreateArticleForm;
+    isPublished: boolean = false;
 
   constructor() { }
 
--- src/app/admin/blog/articles/list/blog.list.article.component.ts
+++ src/app/admin/blog/articles/list/blog.list.article.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../../../entities/article';
 import { ArticleHttpService } from '../../../../services/entities/article.http.service';
+import * as _ from 'lodash';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-list-article',
@@ -8,17 +10,24 @@ import { ArticleHttpService } from '../../../../services/entities/article.http.s
   styleUrls: ['./blog.list.article.component.scss']
 })
 export class BlogListArticleComponent implements OnInit {
-    private articles: Article[];
+    public articles: Article[];
 
-  constructor(private service: ArticleHttpService) { }
+  constructor(private service: ArticleHttpService,
+              private router: Router) { }
 
   ngOnInit() {
       this.service.readAll().subscribe(articles => {
+          console.log(articles);
           return this.articles = articles;
       });
   }
 
     delete(article: Article) {
-        
+      const self = this;
+        this.service.delete(article.ArticleId).subscribe(() => {
+            const index = _.indexOf(this.articles, article);
+            self.articles.splice(index, 1);
+            self.router.navigate(['/admin/experiences']);
+        });
     }
 }
