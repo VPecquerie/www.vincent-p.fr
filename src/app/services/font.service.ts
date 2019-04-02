69c578635b03c2e10066c1778c7b4d8c8eb72358
VPecquerie <vincent.pecquerie@trsb.net>
2019-04-02 13:45:33 +0200

    fix(fonts): resolve issue with service.

--- src/app/services/font.service.ts
+++ src/app/services/font.service.ts
@@ -1,21 +1,27 @@
-import { Injectable } from '@angular/core';
-import * as WebFont from 'webfontloader';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class FontService {
+
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
     public load() {
-        WebFont.load({
-            google: {
-                families: [
-                    'Material Icons',
-                    'Open Sans:300,700',
-                    'Merriweather',
-                    'Montserrat:600',
-                    'Ralewa',
-                    'Lora'
-                ]
-              },
-            timeout: 1500
-        });
+        if (isPlatformBrowser(this.platformId)) {
+            const WebFont = require('webfontloader');
+            WebFont.load({
+                google: {
+                    families: [
+                        'Material Icons',
+                        'Open Sans:300,700',
+                        'Merriweather',
+                        'Montserrat:600',
+                        'Ralewa',
+                        'Lora'
+                    ]
+                },
+                timeout: 1500
+            });
+        }
     }
 }
