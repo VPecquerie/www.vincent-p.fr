2ef63feaf370479a55fe92d674762359c58b6df4
VPecquerie <vincent.pecquerie@trsb.net>
2019-04-09 08:40:11 +0200

    fix(SRI): let gulp generate subresources integrity.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 const gulp = require('gulp');
 const purify = require('gulp-purifycss');
-
+const sriHash = require('gulp-sri-hash');
 gulp.task('purifyCSS', () => {
     return gulp.src('./dist/browser/styles.*.css')
         .pipe(
             purify(
-                ['./src/app/**/*.ts', './src/app/**/*.html'],
+                ['./src/app/**/*.ts', './src/app/**/*.html', './src/index.html'],
                 {
                     info: true, // Outputs reduction information (like in the screenshot above)
                     minify: true, // Minifies the files after reduction
-                    rejected: false, // Logs the CSS rules that were removed
+                    rejected: true, // Logs the CSS rules that were removed
                     whitelist: ['*transition*', '*dimmer*'] // Ignored css classes
                 }
             ),
         )
         .pipe(gulp.dest('./dist/browser/'));
+});
+
+gulp.task('sri', () => {
+    return gulp.src('./src/**/*.html')
+      // do not modify contents of any referenced css- and js-files after this task...
+      .pipe(sriHash())
+      // ... manipulating html files further, is perfectly fine
+      .pipe(gulp.dest('./dist/browser/'));
 });
