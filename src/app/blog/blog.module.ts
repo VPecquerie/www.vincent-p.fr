211c8f1dad9645b775118c3264b9dbcb2ef13f8b
Vincent PECQUERIE <contact@vincent-p.fr>
2019-05-15 08:08:27 +0200

    feat(blog): import routing.

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListArticleComponent } from './list-article/list-article.component';
 import { DetailArticleComponent } from './detail-article/detail-article.component';
+import { BlogRoutingModule } from './blog.routes';
 
 @NgModule({
-  declarations: [ListArticleComponent, DetailArticleComponent ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [ListArticleComponent, DetailArticleComponent],
+    imports: [
+        CommonModule,
+        BlogRoutingModule,
+    ],
 })
-export class BlogModule { }
+export class BlogModule {
+}
